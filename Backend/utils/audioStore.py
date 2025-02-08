161c7fc9355896d2c163f7b6174e83ce3672d2d5
Cindy Fu <fu.s.cindy@gmail.com>
2025-02-08 11:06:56 -0500

    some audio recognition done

--- Backend/utils/audioStore.py
+++ Backend/utils/audioStore.py
@@ -1,2 +1,33 @@
+import speech_recognition as sr
+import time
+
+# TODO:
+# store text locally for redundancy
+# make this function also send to api
+
+listen_len = 20   # may change later
+
+# transcribes at most listen_len seconds of speech, then sends it to the perplexity api
 def transcribeAudio():
-    pass
+    # Initialize recognizer class (for recognizing the speech)
+    r = sr.Recognizer()
+    # list_of_phrases = []
+    list_of_phrases_raw = []
+    # arr_of_phrases = [None] * 5
+    # i = 0 # index into arr_of_phrases
+
+    init_time = time.time()
+    while (time.time() - init_time < listen_len):
+        with sr.Microphone() as source:
+            print("Talk")  # keeping these print statements here for debugging purposes
+            r.pause_threshold = 0.8
+            list_of_phrases_raw.append(r.listen(source, phrase_time_limit=10.0))  #good?
+            print("Time over, thanks")
+
+    list_of_phrases_text = []
+    for raw in list_of_phrases_raw:
+        try:
+            list_of_phrases_text.append(r.recognize_google(raw))
+        except:
+            print("Sorry, I did not get that")
+
