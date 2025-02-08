daa9a3098067aa0442471ee8cd8017a05a8e0512
Elizabetht1 <eterveen@andrew.cmu.edu>
2025-02-08 12:08:24 -0500

    updated networking, just trying to get it to compile at this point

--- Frontend/Assets/Code/Scripts/APIManager.cs
+++ Frontend/Assets/Code/Scripts/APIManager.cs
@@ -8,15 +8,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* NETOWRKING PACKAGES */
+using UnityEngine.Networking;
+
+
 public class APIManager : MonoBehaviour
 {
     // events
     public class NPCResponseArgs { public AudioClip audioClip; }
     public event EventHandler<NPCResponseArgs> OnNPCResponse;
 
+
+    //HTTP client 
+    public string ApiAdress = "http://127.0.0.1:8000";
+
+    private class GenConvoReq{
+        private string agentRole;
+        private string agentTone;
+        private string userRole;
+        private string userQuery;
+    }
+
     [SerializeField] private Player player;
 
     private void Start() {
+        Debug.Log("I am alive!");
         player.OnPlayerSpoke += OnPlayerSpoke;
     }
 
@@ -33,4 +49,43 @@ public class APIManager : MonoBehaviour
         // fire event to make NPC respond
         OnNPCResponse?.Invoke(this, new NPCResponseArgs { audioClip = clip });
     }
+
+    /**
+    Make a request to generate a question to the backend
+    */
+    private IEnumerator postGenQuestionReq() {
+        // GenConvoReq convoReq = new GenConvoReq{
+        //         agentRole = "interviewer",
+        //         agentTone = "neutral",
+        //         userRole = "interviewee",
+        //         userQuery = "give me a job pretty please",
+        // };
+
+        // Create a Web Form
+        Dictionary<string, string> genConvoReqBody = new();
+        genConvoReqBody["agentRole"] = "interviewer";
+        genConvoReqBody["agentTone"] = "neutral";
+        genConvoReqBody["userRole"] = "interviewee";
+        genConvoReqBody["userQuery"] = "give me a job pretty please";
+        
+
+        using (UnityWebRequest speechApiReq = UnityWebRequest.Post(ApiAdress,genConvoReqBody)) {
+            yield return speechApiReq.SendWebRequest();
+            
+           if (speechApiReq.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError(speechApiReq.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
+
+        }
+
+    }
+
+
+
 }
+
