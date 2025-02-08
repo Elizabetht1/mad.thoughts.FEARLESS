c71099f90b63c79989e52df272629cc5ab863490
Elizabetht1 <eterveen@andrew.cmu.edu>
2025-02-08 13:14:01 -0500

    updated baisic functionality

--- Frontend/Assets/Code/Scripts/APIManager.cs
+++ Frontend/Assets/Code/Scripts/APIManager.cs
@@ -20,13 +20,13 @@ public class APIManager : MonoBehaviour
 
 
     //HTTP client 
-    private string ApiAdress = "http://127.0.0.1:8000";
+    private string ApiAdress = "http://127.0.0.1:8000/gen-convo";
 
-    private class GenConvoReq{
-        private string agentRole;
-        private string agentTone;
-        private string userRole;
-        private string userQuery;
+    public class GenConvoReq{
+        public string agentRole;
+        public string agentTone;
+        public string userRole;
+        public string userQuery;
     }
 
     [SerializeField] private Player player;
@@ -55,34 +55,58 @@ public class APIManager : MonoBehaviour
     Make a request to generate a question to the backend
     */
     private IEnumerator postGenQuestionReq() {
-        // GenConvoReq convoReq = new GenConvoReq{
-        //         agentRole = "interviewer",
-        //         agentTone = "neutral",
-        //         userRole = "interviewee",
-        //         userQuery = "give me a job pretty please",
-        // };
-
-        // Create a Web Form
-        Dictionary<string, string> genConvoReqBody = new();
-        genConvoReqBody["agentRole"] = "interviewer";
-        genConvoReqBody["agentTone"] = "neutral";
-        genConvoReqBody["userRole"] = "interviewee";
-        genConvoReqBody["userQuery"] = "give me a job pretty please";
         
+        GenConvoReq convoReq = new GenConvoReq{
+                agentRole = "interviewer",
+                agentTone = "neutral",
+                userRole = "interviewee",
+                userQuery = "give me a job pretty please",
+        };
+        string json = JsonUtility.ToJson(convoReq);
+        var req = new UnityWebRequest(ApiAdress, "POST");
 
-        using (UnityWebRequest speechApiReq = UnityWebRequest.Post(ApiAdress,genConvoReqBody)) {
-            yield return speechApiReq.SendWebRequest();
-            
-           if (speechApiReq.result != UnityWebRequest.Result.Success)
-            {
-                Debug.LogError(speechApiReq.error);
-            }
-            else
-            {
-                Debug.Log("Form upload complete!");
-            }
 
+        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+        
+        
+        req.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+        req.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        req.SetRequestHeader("Content-Type", "application/json");
+
+        //Send the request then wait here until it returns
+        yield return req.SendWebRequest();
+
+        if (req.isNetworkError)
+        {
+                Debug.Log("Error While Sending: " + req.error);
+        }else{
+            Debug.Log("Received: " + req.downloadHandler.text);
         }
+
+        // // Create a Web Form
+        // WWWForm form = new WWWForm();
+        // form.AddField("agentRole", "interviewer");
+        // form.AddField("agentTone", "neutral");
+        // form.AddField("userRole", "interviewee");
+        // form.AddField("userQuery","give me a job pretty please");
+
+
+
+
+        // using (UnityWebRequest speechApiReq = UnityWebRequest.Post(ApiAdress,form)) {
+        //     yield return speechApiReq.SendWebRequest();
+        //     Debug.Log("receiving a response \n");
+
+        //    if (speechApiReq.result != UnityWebRequest.Result.Success)
+        //     {
+        //         Debug.LogError(JsonUtility.ToJson(speechApiReq.result));
+        //     }
+        //     else
+        //     {
+        //         Debug.Log("Form upload complete!");
+        //     }
+
+        // }
 
     }
 
